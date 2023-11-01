731d68ceed472f815cd3d774d0f74b9c5b07a5c3
Bogdan Popa <bogdan@defn.io>
2023-11-01 21:16:52 +0200

    Racket,backend: make response writing more reliable
    Serializing fields may fail if passed invalid or wrong data. This
    change makes it possible to detect and return error responses in those
    cases, without introducing too much overhead, if the serialization
    errors occur within the first 1MB of serialized data.

--- Racket/noise-serde-lib/backend.rkt
+++ Racket/noise-serde-lib/backend.rkt
@@ -2,6 +2,7 @@
 
 (require ffi/unsafe/port
          racket/match
+         racket/port
          "private/backend.rkt"
          "private/callout.rkt"
          "private/debug.rkt"
@@ -36,31 +37,22 @@
                   (close-output-port server-out)]
                  [`(response ,id ,response-type ,response-data)
                   (log-noise-debug "response ~a: ~.v" id response-data)
-                  (with-handlers ([exn:fail?
-                                   (λ (e)
-                                     ((error-display-handler)
-                                      (format "backend/serve write failed: ~a" (exn-message e))
-                                      e))])
-                    (write-uvarint id server-out)
-                    (cond
-                      [(exn:fail? response-data)
-                       (write-byte 0 server-out)
-                       (write-field String (exn-message response-data) server-out)]
-                      [else
-                       (write-byte 1 server-out)
-                       (unless (eq? response-type Void)
-                         (write-field response-type response-data server-out))])
-                    (flush-output server-out))
+                  (write-uvarint id server-out)
+                  (if (exn:fail? response-data)
+                      (write-error response-data server-out)
+                      (write-data response-type response-data server-out))
+                  (flush-output server-out)
                   (loop)]
                  [msg
                   (log-warning "backend/serve invalid message: ~e" msg)])))
             (handle-evt
              server-in
              (lambda (in)
-               (with-handlers ([exn:fail? (λ (e)
-                                            ((error-display-handler)
-                                             (format "backend/serve: ~a" (exn-message e))
-                                             e))])
+               (with-handlers ([exn:fail?
+                                (lambda (e)
+                                  ((error-display-handler)
+                                   (format "backend/serve: ~a" (exn-message e))
+                                   e))])
                  (define req-id (read-uvarint in))
                  (define rpc-id (read-uvarint in))
                  (match-define (rpc-info _id rpc-name rpc-args response-type handler)
@@ -77,7 +69,7 @@
                       (lambda ()
                         (define response-data
                           (with-handlers ([exn:fail?
-                                           (λ (e)
+                                           (lambda (e)
                                              (begin0 e
                                                ((error-display-handler)
                                                 (format "backend/handler: ~a" (exn-message e))
@@ -95,6 +87,77 @@
     (thread-send thd '(stop))
     (thread-wait thd)
     (custodian-shutdown-all cust)))
+
+(define (write-error e out)
+  (write-byte 0 out)
+  (write-field String (~stacktrace e) out))
+
+;; Use a background thread to write the data in order to try and catch
+;; any errors that occur during writing without having to buffer writes
+;; into Racket memory before sending them over to the client.
+(define (write-data type data [out (current-output-port)])
+  (define err-ch (make-channel))
+  (define-values (pipe-in pipe-out)
+    (make-pipe (* 1 1024 1024)))
+  (define write-thd
+    (thread
+     (lambda ()
+       (with-handlers ([exn:fail? (λ (e) (channel-put err-ch e))])
+         (unless (eq? type Void)
+           (write-field type data pipe-out)))
+       (close-output-port pipe-out))))
+  (let write-loop ([status-sent? #f])
+    (sync
+     (handle-evt
+      err-ch
+      (lambda (e)
+        (when status-sent?
+          ;; We've already written some response data, so we MUST crash
+          ;; in order to avoid having our response be misinterpreted.
+          (exit e))
+        (write-error e out)))
+     (handle-evt
+      write-thd
+      (lambda (_)
+        (unless status-sent?
+          (write-byte 1 out))
+        (copy-port pipe-in out)))
+     (handle-evt
+      pipe-in
+      (lambda (_)
+        (unless status-sent?
+          (write-byte 1 out))
+        (copy-port pipe-in out)
+        (write-loop #t)))))
+  (close-input-port pipe-in))
+
+(define (~stacktrace e)
+  (call-with-output-string
+   (lambda (out)
+     (parameterize ([current-error-port out])
+       ((error-display-handler)
+        (exn-message e)
+        e)))))
+
+(module+ test
+  (require rackunit)
+  (define (response-bytes type data)
+    (with-output-to-bytes
+      (lambda ()
+        (write-data type data))))
+  (check-equal?
+   (response-bytes UInt32 1)
+   #"\1\0\0\0\1")
+  (check-equal?
+   (response-bytes String "hello")
+   #"\1\nhello")
+  (check-true
+   (regexp-match?
+    #rx#"string->bytes/utf-8: contract violation"
+    (response-bytes String #f)))
+  (check-equal?
+   (response-bytes (Optional String) #f)
+   #"\1\0"))
 
 
 ;; callout ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
