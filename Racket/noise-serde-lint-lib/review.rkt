34f576880e88488cd518d8e8b841fe7dc44d6794
Bogdan Popa <bogdan@defn.io>
2023-12-31 17:22:41 +0200

    Racket,lint: add linters for define-enum and define-record

--- Racket/noise-serde-lint-lib/review.rkt
+++ Racket/noise-serde-lint-lib/review.rkt
@@ -12,9 +12,49 @@
 (define-expression-syntax-class expression)
 
 (define (should-review-syntax? stx)
-  (syntax-case stx (define-rpc)
+  (syntax-case stx (define-enum define-record define-rpc)
+    [(define-enum . _rest) #t]
+    [(define-record . _rest) #t]
     [(define-rpc . _rest) #t]
     [_ #f]))
+
+(define-syntax-class enum-definition
+  #:datum-literals (define-enum :)
+  (pattern (define-enum
+             ~!
+             enum-id:id
+             [variant-id:id {field-id:id : field-type:expression} ...] ...)
+           #:do [(track-binding #'enum-id #:check-usages? #f)
+                 (track-binding #'enum-id "~a?" #:check-usages? #f)
+                 (define enum-id-sym (syntax->datum #'enum-id))
+                 (for ([variant-id-stx (in-list (syntax-e #'(variant-id ...)))])
+                   (track-binding
+                    variant-id-stx
+                    #:related-to #'enum-id
+                    #:check-usages? #f
+                    (format "~a.~~a" enum-id-sym)))]))
+
+(define-syntax-class record-field
+  (pattern [id:id : type-expr:expression . opts])
+  (pattern [(id:id default-expr:expression) : type-expr:expression . opts]))
+
+(define-syntax-class record-definition
+  #:datum-literals (define-record :)
+  (pattern (define-record
+             ~!
+             record-id:id
+             record-field:record-field ...+)
+           #:do [(track-binding #'record-id #:check-usages? #f)
+                 (track-binding #'record-id "~a?" #:check-usages? #f)
+                 (track-binding #'record-id "make-~a" #:check-usages? #f)
+                 (define record-id-sym (syntax->datum #'record-id))
+                 (for ([record-field-id-stx (in-list (syntax-e #'(record-field.id ...)))])
+                   (for ([p (in-list '("" "set-"))])
+                     (track-binding
+                      record-field-id-stx
+                      #:related-to #'record-id
+                      #:check-usages? #f
+                      (format "~a~a-~~a" p record-id-sym))))]))
 
 (define-syntax-class rpc-arg
   #:datum-literals (:)
@@ -35,4 +75,6 @@
 
 (define (review-syntax stx)
   (syntax-parse stx
+    [d:enum-definition #'d]
+    [d:record-definition #'d]
     [d:rpc-definition #'d]))
