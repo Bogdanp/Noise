a821a726d4a6a3e3c910a2c5ae0f34bcdbbcbd9d
Bogdan Popa <bogdan@defn.io>
2025-01-15 08:01:08 +0200

    Racket,build: bump version

--- Racket/noise-serde-lib/info.rkt
+++ Racket/noise-serde-lib/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define license 'BSD-3-Clause)
-(define version "0.9.1")
+(define version "0.9.2")
 (define collection "noise")
 (define deps '("base"
                "threading-lib"))
